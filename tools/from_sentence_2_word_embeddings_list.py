e32977d60193f6668db85380cef68afc03713f9d
2305932025@qq.com <2305932025@qq.com>
2022-01-07 08:18:22 +0800

    let 300<->343 easier

--- tools/from_sentence_2_word_embeddings_list.py
+++ tools/from_sentence_2_word_embeddings_list.py
@@ -7,17 +7,20 @@ from nltk.tree import Tree
 print("___________****************************************** 300 ********************************************************************************")
 
 def from_sentence_2_word_embeddings_list(sentence, stanford_nlp, word2vec_vocab):
+    if '%' in sentence:
+        sentence = sentence.replace('%', '%25')
+
+    words_list = stanford_nlp.word_tokenize(sentence)
+
+    # ################################################################### 343 ##################################################
     # ne_type_list = ['ORGANIZATION', 'LOCATION', 'PERSON', 'MONEY', 'PERCENT', 'DATE', 'TIME']
     # pos_tag_type_list = ['CC', 'CD', 'DT', 'EX', 'FW', 'IN', 'JJ', 'JJR', 'JJS', 'LS', 'MD', 'NN', 'NNS', 'NNP', 'NNPS',
     #                      'PDT', 'POS', 'PRP', 'PRP$', 'RB', 'RBR', 'RBS', 'RP', 'SYM', 'TO', 'UH', 'VB', 'VBD', 'VBG',
     #                      'VBN', 'VBP', 'VBZ', 'WDT', 'WP', 'WP$', 'WRB']
-
-    if '%' in sentence:
-        sentence = sentence.replace('%', '%25')
-
+    #
     # n_list = stanford_nlp.ner(sentence)
     # p_list = stanford_nlp.pos_tag(sentence)
-    words_list = stanford_nlp.word_tokenize(sentence)
+    ##############################################################################################################################
 
     word_embeddings_list = []
     for i in range(len(words_list)):
@@ -28,6 +31,7 @@ def from_sentence_2_word_embeddings_list(sentence, stanford_nlp, word2vec_vocab)
         else:
             word_embedding = torch.rand(300) / 2 - 0.25
 
+        # ################################################################### 343 ##################################################
         # word_pos_one_hot = [0 for _ in range(36)]
         # p = p_list[i][1]
         # if p in pos_tag_type_list:
@@ -41,6 +45,7 @@ def from_sentence_2_word_embeddings_list(sentence, stanford_nlp, word2vec_vocab)
         #     ne_one_hot[ne_type_list.index(n)] = 1
         # ne_one_hot = torch.tensor(ne_one_hot)
         # word_embedding = torch.cat((word_embedding, ne_one_hot))
+        # #####################################################################################################################
 
         word_embeddings_list.append(word_embedding)
     word_embeddings_list = torch.stack(word_embeddings_list)
